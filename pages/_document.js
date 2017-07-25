b17bcbe33f0bc02e57c4630bd97b4c3e1b1e873c
Andres Calabrese <acalabrese@gmail.com>
2017-07-25 18:14:20 -0300

    Add nextjs with ant design and custom theme example

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import flush from 'styled-jsx/server';
+import enUS from 'antd/lib/locale-provider/en_US';
+import { LocaleProvider } from 'antd';
+import stylesheet from 'styles/index.less';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const { html, head } = renderPage();
+    const styles = flush();
+    return { html, head, styles };
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+        </Head>
+        <body>
+          <LocaleProvider locale={enUS}>
+            <Main />
+          </LocaleProvider>
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
