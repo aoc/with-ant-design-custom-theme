b675db786733514d37d34a77d8d158eb6c2232f2
Johnwook Choi <johnwook.choi@huiseoul.com>
2018-01-30 22:06:07 +0900

    Update _document.js
    - return newly added initialProps
      - see detail in https://github.com/zeit/next.js#custom-document
    - remove unnecessary jsx and global props in style tag
      - it just works well without props. You have to refresh page to
      apply customzized style

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,21 @@
-import Document, { Head, Main, NextScript } from 'next/document';
-import flush from 'styled-jsx/server';
-import enUS from 'antd/lib/locale-provider/en_US';
-import { LocaleProvider } from 'antd';
-import stylesheet from 'styles/index.less';
+import Document, { Head, Main, NextScript } from "next/document";
+import flush from "styled-jsx/server";
+import enUS from "antd/lib/locale-provider/en_US";
+import { LocaleProvider } from "antd";
+import stylesheet from "styles/index.less";
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
-    const { html, head } = renderPage();
+    const { html, head, errorHtml, chunks } = renderPage();
     const styles = flush();
-    return { html, head, styles };
+    return { html, head, errorHtml, chunks, styles };
   }
 
   render() {
     return (
       <html lang="en">
         <Head>
-          <style jsx global>{ stylesheet }</style>
+          <style>{stylesheet}</style>
         </Head>
         <body>
           <LocaleProvider locale={enUS}>
